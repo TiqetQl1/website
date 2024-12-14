25a0658f370ee7382a8c910d33e9206d3c47214a
Kian Arabi <k80arabi@outlook.com>
2024-12-15 01:50:13 +0330

    Adds a function to make contracts instances

--- src/utils/ether.ts
+++ src/utils/ether.ts
@@ -1,16 +1,14 @@
 import { ethers } from "ethers"
-import { formatBalance } from ".";
 import {address, chainData} from "@/statics/poolMakerData";
 import PoolMakerABI from "@/statics/PoolMakerABI";
+import PoolABI from "@/statics/PoolABI";
 
 export const provider = new ethers.JsonRpcProvider(chainData.rpcUrls[0])
 export const poolMakerContract = new ethers.Contract(address, PoolMakerABI, provider);
 
-// export const retriveBalance = async (address: string) => {
-//     const balance = (await provider.getBalance(address))
-//     console.log(formatBalance(balance))
-// }
-
-export const getActivePools = async () : Promise<string[]> => {
-    return (await poolMakerContract.all_actives())
+// The wallet should provide signer !!
+export const getMyContract = async (wallet: EIP6963ProviderDetail, contract_address, ABI) => {
+    const etherProv = new ethers.BrowserProvider(wallet.provider)
+    const signer    = await etherProv.getSigner()
+    return new ethers.Contract(contract_address, ABI, signer)
 }
