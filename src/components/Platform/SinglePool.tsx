f444ab82e0922966ad98334ffdc689b640b4d751
Kian Arabi <k80arabi@outlook.com>
2024-12-24 02:25:02 +0330

    Progressing, just commiting to be saved

--- src/components/Platform/SinglePool.tsx
+++ src/components/Platform/SinglePool.tsx
@@ -1,7 +1,8 @@
 import usePool from "@/hooks/usePool"
+import styles from "./Platform.module.css"
 import { FC, useEffect, useRef, useState } from "react"
 import { Configs, Results, States, usePoolReturnType } from "@/types/Pool"
-import { suppressDecodeError } from "@/utils"
+import { bigIntToFixed, formatAddress, formatTime, suppressDecodeError, TimeSegments } from "@/utils"
 
 interface SinglePoolGuard {
     wallet?: EIP6963ProviderDetail,
@@ -9,16 +10,29 @@ interface SinglePoolGuard {
     text?: string
 }
 
-const SinglePool : FC<SinglePoolGuard> = ({wallet=null, pool_address="0x0000000000000000000000000000000000000000", text=null}) => {
+const SinglePool 
+    : FC<SinglePoolGuard> 
+    = ({
+        wallet=null, 
+        pool_address="0x0000000000000000000000000000000000000000", 
+        text=null
+    }) => {
     const {pool, buy, getMyTicketsCount} : usePoolReturnType = usePool(pool_address)
     const [configs, setConfigs] = useState<Configs>()
     const [results, setResults] = useState<Results>()
     const [states, setStates]   = useState<States>()
     const [myTickets, setMyTickets] = useState<bigint>(0n)
+    const [poolTotal, setPoolTotal] = useState<bigint>(0n)
+    const [remTime, setRemTime] = useState<TimeSegments>()
     const countInputRef = useRef(null)
 
     const reloadInfo = (getX:()=>Promise<any>, setX) => getX().then(res=>setX(res)).catch(err=>suppressDecodeError(err))
     const reloadMyTickets = () => getMyTicketsCount(wallet).then(res=>setMyTickets(res))
+    const reloadTimeLeft = () => {
+        const now = parseInt((Date.now()/1000).toFixed())
+        const secondsRem = (configs?.time_end ?  parseInt(configs.time_end.toString()) : now )-now
+        setRemTime(formatTime(secondsRem))
+    }
 
     const buyHandler = async () => {
         await buy(wallet, countInputRef.current.value, configs.ticket_price_usdt)
@@ -29,19 +43,62 @@ const SinglePool : FC<SinglePoolGuard> = ({wallet=null, pool_address="0x00000000
         reloadInfo(pool.configs, setConfigs)
         reloadInfo(pool.results, setResults)
         reloadInfo(pool.states, setStates)
-        reloadMyTickets()
-        setInterval(()=>reloadInfo(pool.states, setStates),10000)
-    },[,wallet])
+        reloadInfo(pool.contract.poolTotal, setPoolTotal)
+        let getStatesInterval = setInterval(()=>{
+            reloadInfo(pool.states, setStates)
+            reloadInfo(pool.contract.poolTotal, setPoolTotal)
+        },10000)
+        let remTimeInterval = setInterval(reloadTimeLeft,1000)
+        return ()=>{
+            clearInterval(getStatesInterval)
+            clearInterval(remTimeInterval)
+        }
+    },[])
 
     useEffect(()=>{
-        console.log(myTickets)
-    },[myTickets])
+        reloadMyTickets()
+    },[,wallet])
+
 
     return (
-        <div className="singlePool">
-            {text || pool_address}
-            <br />
-            {configs && configs.organizer}
+        <div className={styles.singlePool}>
+                {/* Total raised money */}
+                <div className={styles.total}>
+                    {
+                    ( states?.stage_ && states.stage_ != 0n )
+                    ?
+                    <>
+                        <div>
+                            Total raised :&nbsp;
+                        </div>
+                        <div className={styles.money}>
+                            {bigIntToFixed(poolTotal, 6)+'$'}
+                        </div>
+                    </>
+                    :
+                    <>
+                        <div>
+                            Not started&nbsp;
+                        </div>
+                        <div>
+                            YET
+                        </div>
+                    </>
+                    }
+                </div>
+            <div>
+                {states?.stage_ && states.stage_.toString()}
+            </div>
+            <div className={styles.countdown}>
+                {(Date.now()/1000).toFixed()}<br/>
+                <br />
+                {configs && configs.time_end.toString()}
+                <br />
+                {remTime && remTime.days+':'+remTime.hours+':'+remTime.minutes+':'+remTime.seconds}
+            </div>
+            <div className={styles.address}>
+                {text || formatAddress(pool_address, 5)}
+            </div>
         </div>
     )
 }
