d8fbb101e1430e32e34e1800d2c61bd939f87fd9
Kian Arabi <k80arabi@outlook.com>
2024-12-29 17:00:44 +0330

    Makes dot use `useData` instead of useState

--- src/components/Platform/Dot.tsx
+++ src/components/Platform/Dot.tsx
@@ -1,3 +1,4 @@
+import useData from "@/hooks/useData"
 import { Pool, States } from "@/types/Pool"
 import { suppressDecodeError } from "@/utils"
 import { FC, useEffect, useState } from "react"
@@ -9,19 +10,19 @@ type DotGuard = {
 }
 
 const Dot : FC<DotGuard> = ({pool , isActive, clickHandler}) => {
-    const [states, setStates] = useState<States>()
+    const [states, isLoading, retry] = useData<States>(pool.states, 5)
+
+    useEffect(()=>{
+        if (!isLoading && !(states?.stage_)) {
+            setTimeout(retry, 10000)
+        }
+    },[isLoading])
+
     const transform = isActive?"scale(1.5)":"scale(1.0)"
     const backgroundColor = 
         (!states?.stage_||states.stage_==0n) 
             ? "var(--box-shadow)" 
             : ( states.stage_==1n ? "var(--yellow)" : "var(--green)" )
-
-    useEffect(()=>{
-        pool
-            .states()
-            .then(res=>setStates(res))
-            .catch(err=>suppressDecodeError(err))
-    },[])
 
     return (
         <div
