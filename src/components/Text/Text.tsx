f373cef94376a6a82d72978f1354741502aa8d8e
Kian Arabi <k80arabi@outlook.com>
2025-02-01 19:16:58 +0330

    Changes as QioShy wanted

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -4,15 +4,20 @@ import styles from "./Text.module.css"
 const Text : FC = () => {
 
     return <section className={styles.sec}>
-        <p className={styles.p}>
+        <div className={styles.p}>
+            <p>
             You always find yourself thinking, “Come on, this was so obvious!” or “I knew this would happen!” Maybe even, “Why didn’t they just do this?”
-            <br />
-            Well, now’s your chance to turn those predictions into real money. Bet on real-world events and multi-day lotteries, outsmart the competition, and prove you were right all along. Didn’t win this time? No worries—there’s always another opportunity just around the corner. Built on the fast, low-cost, and powerful <b>QL1</b> blockchain, our platform ensures complete transparency and security, so you can play with confidence.
-            <br />
-            Fuel your dreams, justify your losses, calm your doubts, confirm your instincts, and take your shot at victory.
-            <br />
-            All it takes is just one <b>TiQet</b> in your hand!
-        </p>
+            </p>
+            <p>
+                Well, now’s your chance to turn those predictions into real money. Bet on real-world events and multi-day lotteries, outsmart the competition, and prove you were right all along. Didn’t win this time? No worries—there’s always another opportunity just around the corner. Built on the fast, low-cost, and powerful <b>QL1</b> blockchain, our platform ensures complete transparency and security, so you can play with confidence.
+            </p>
+            <p>
+                Fuel your dreams, justify your losses, calm your doubts, confirm your instincts, and take your shot at victory.
+            </p>
+            <p>
+                <b>All it takes is just one TiQet in your hand!</b>
+            </p>
+        </div>
     </section>
 }
 
