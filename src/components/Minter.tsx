9a336a8b23927ac90618e581193536b8a36ba6ca
Kian Arabi <k80arabi@outlook.com>
2025-02-01 15:36:14 +0330

    Fix : wrong hierarchy in Minter

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -20,14 +20,16 @@ const Minter : FC = () => {
             borderRadius: "28px"
     }
 
-    return <iframe
-        src="https://embed.ipfscdn.io/ipfs/bafybeicd3qfzelz4su7ng6n523virdsgobrc5pcbarhwqv3dj3drh645pi/?contract=0x01c6acBC7E8DBD0a2256d0a769d046Cec92E248C&chain=%7B%22name%22%3A%22QL1%22%2C%22chain%22%3A%22QOM%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Frpc.qom.one%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Shiba+Predator%22%2C%22symbol%22%3A%22QOM%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22qom%22%2C%22chainId%22%3A766%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22ql1%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmRc1kJ7AgcDL1BSoMYudatWHTrz27K6WNTwGifQb5V17D%22%2C%22width%22%3A518%2C%22height%22%3A518%2C%22format%22%3A%22png%22%7D%7D&clientId=9e00bba0c3e8e88d1191bdd28e57bc45&theme=light&primaryColor=orange"
-        title='NFT minter'
-        aria-label='NFT minter'
-        style={styles}
-        height={styles.height}
-        width={styles.width}
-        frameBorder="0"
-    ></iframe>
+    return <section>
+        <iframe
+            src="https://embed.ipfscdn.io/ipfs/bafybeicd3qfzelz4su7ng6n523virdsgobrc5pcbarhwqv3dj3drh645pi/?contract=0x01c6acBC7E8DBD0a2256d0a769d046Cec92E248C&chain=%7B%22name%22%3A%22QL1%22%2C%22chain%22%3A%22QOM%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Frpc.qom.one%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Shiba+Predator%22%2C%22symbol%22%3A%22QOM%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22qom%22%2C%22chainId%22%3A766%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22ql1%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmRc1kJ7AgcDL1BSoMYudatWHTrz27K6WNTwGifQb5V17D%22%2C%22width%22%3A518%2C%22height%22%3A518%2C%22format%22%3A%22png%22%7D%7D&clientId=9e00bba0c3e8e88d1191bdd28e57bc45&theme=light&primaryColor=orange"
+            title='NFT minter'
+            aria-label='NFT minter'
+            style={styles}
+            height={styles.height}
+            width={styles.width}
+            frameBorder="0"
+        ></iframe>
+    </section>
 }
 export default Minter
